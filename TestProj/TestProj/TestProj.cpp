71ee0838f0e7fe9003e70c0aa02a1db97aacf4a0
iADx8 <iADx8@yandex.ru>
2017-07-03 17:19:59 +0400

    Init

---
+++ TestProj/TestProj/TestProj.cpp
@@ -0,0 +1,15 @@
+// TestProj.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "1" << endl;
+	getchar();
+    return 0;
+}
+
